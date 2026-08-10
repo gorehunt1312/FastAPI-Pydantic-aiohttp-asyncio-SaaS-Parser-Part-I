8873c8fcb5120ca4b71b3f4dd409fd4b9913e6fd
gorehunt1312 <gorehunt1312@gmail.com>
2026-08-11 00:34:45 +0300

    FastAPI+Pydantic+aiohttp+asyncio SaaS-Parser Part I

---
+++ client.py
@@ -0,0 +1,46 @@
+import aiohttp
+import asyncio
+from fake_useragent import UserAgent
+from bs4 import BeautifulSoup
+
+ua = UserAgent()
+headers = {
+    "User-Agent": ua.random,
+    "Accept": "application/json, text/plain, */*",
+    "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+}
+
+async def send_data():
+    semaphore = asyncio.Semaphore(2)
+    async with aiohttp.ClientSession() as session:
+        urls = [f"https://books.toscrape.com/catalogue/page-{page}.html" for page in range(1, 4)]
+        tasks = [dev(url, session, semaphore) for url in urls]
+        await asyncio.gather(*tasks)
+async def dev(url, session, semaphore):
+        try:
+            async with semaphore:
+                async with session.get(url, headers=headers) as response:
+                    if response.status == 200:
+                        print(f'Подключение успешно! {response.status}')
+                        html = await response.text()
+                        soup = BeautifulSoup(html, 'html.parser')
+                        html_data = soup.find_all('article', class_="product_pod")
+                    for get_html_data in html_data:
+                        product_id = get_html_data.find('h3').find('a')["title"]
+                        price = get_html_data.find('p', class_='price_color').text
+                        clean_price = float(price.replace('£', '').strip())
+                        article = product_id[:10].strip().replace(" ", "_")
+                        async with session.post("http://127.0.0.1:8000/api/v1/ingest", json={
+                            "product_id": article,
+                            "price": clean_price,
+                            "category": "Books",
+                            "discount": 0
+                        }) as response:
+                            print(f"Книга '{article}' отправлена. Статус: {response.status}")
+                            print(f"Ответ от FastAPI: {await response.json()}")
+                    else:
+                        raise Exception(f'Сайт-донор вернул ошибку. Статус: {response.status}')
+        except Exception as e:
+            print(f'Критическая ошибка. {e}')
+if __name__ == "__main__":
+    asyncio.run(send_data())
---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, Response, status
+from pydantic import BaseModel, Field, EmailStr, HttpUrl, ConfigDict, field_validator, model_validator, ValidationError  
+from typing import Optional, List, Dict, Union
+from datetime import datetime, date
+
+app = FastAPI(
+    title="app",
+    description="without description",
+    version="1.0.0",
+    docs_url="/api/v1/docs",
+    redoc_url="/api/v1/redoc",
+    openapi_url="/api/v1/openapi.json"
+)
+
+class ProductSchema(BaseModel):
+    product_id: str = Field(min_lenght=5, max_length=12)
+    price: float = Field(ge=10.0, le=1000000.0)
+    category: str = "unknown"
+    discount: int = Field(ge=0, le=90)
+    empty_fields: Optional[str] = None
+
+    model_config = ConfigDict(
+        str_strip_whitespace=True,
+        extra="forbid",
+        populate_by_name=True
+    )
+
+@app.post(
+    "/api/v1/ingest",
+    status_code=status.HTTP_201_CREATED,
+    response_model_exclude_unset=True
+)
+async def ready_data(payload: ProductSchema, response: Response):
+    response.headers["X-App-Status"] = "Verified"
+    return payload
